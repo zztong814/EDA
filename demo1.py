4dd5edef2c4724a1ac842d6f362e69ebe510b055
zztong814 <2718213676@qq.com>
2025-10-13 16:00:56 +0800

    Update demo1.py

--- demo1.py
+++ demo1.py
@@ -1,1 +1,9 @@
+from tqdm import tqdm
+import time
+from train.joint import Joint_train
 
+if __name__ == '__main__':
+    for i in tqdm(range(100), desc="Training Progress", ncols=100):
+        time.sleep(0.01)  # 模拟耗时
+
+    Joint_train()
